b4fb378e1a9e30ad0a1a769d4b238a2cb4973655
Igben-Nehemiah <igbennehemiah@gmail.com>
2021-02-17 11:39:40 +0100

    Linked up the Create account window, share project window and create project window to the menu pane control.

--- WPFGraphicUserInterface/Views/MenuPaneView.xaml.cs
+++ WPFGraphicUserInterface/Views/MenuPaneView.xaml.cs
@@ -18,9 +18,39 @@ namespace WPFGraphicUserInterface.Views
     /// </summary>
     public partial class MenuPaneView : UserControl
     {
+        private CreateAccountWindowView CreateAccountWindowView { get; set; }
+        private CreateProjectWindowView CreateProjectWindowView { get; set; }
+        private ShareProjectWindowView ShareProjectWindowView { get; set; }
+
         public MenuPaneView()
         {
             InitializeComponent();
+        }
+
+        private void CreateAccount_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (CreateAccountWindowView != null) { CreateAccountWindowView = null; }
+            CreateAccountWindowView = new CreateAccountWindowView();
+            CreateAccountWindowView.Show();
+        }
+
+        private void CreateProject_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (CreateProjectWindowView != null) { CreateProjectWindowView = null; }
+            CreateProjectWindowView = new CreateProjectWindowView();
+            CreateProjectWindowView.Show();
+        }
+
+        private void ShareProject_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (ShareProjectWindowView != null) { ShareProjectWindowView = null; }
+            ShareProjectWindowView = new ShareProjectWindowView();
+            ShareProjectWindowView.Show();
+        }
+
+        private void MenuPaneViewClose_Click(object sender, MouseButtonEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
         }
     }
 }
